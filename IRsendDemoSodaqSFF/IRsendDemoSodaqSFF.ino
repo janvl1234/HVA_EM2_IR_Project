58363aed6ff4a3e7a412ae1464ba77b1d33e40e8
Jan van Loenen <git@janvanloenen.nl>
2020-02-25 10:59:35 +0100

    IRsendDemoSodaqSFF

---
+++ IRsendDemoSodaqSFF/IRsendDemoSodaqSFF.ino
@@ -0,0 +1,32 @@
+/*
+ * IRremote: IRsendDemo - demonstrates sending IR codes with IRsend
+ * An IR LED must be connected to Arduino PWM pin 3.
+ * Version 0.1 July, 2009
+ * Copyright 2009 Ken Shirriff
+ * http://arcfn.com
+ */
+
+
+#include "IRremote.h"
+
+IRsend irsend;
+
+void setup()
+{
+}
+
+void loop() {
+    for (int i = 0; i < 3; i++) {
+        irsend.sendSAMSUNG(0x0061616e, 32);
+    // 61616e = aan 
+        delay(40);
+    }
+    delay(5000); //5 second delay between each signal burst
+
+    for (int i = 0; i < 3; i++) {
+    irsend.sendSAMSUNG(0x00756974, 32);
+        // 756974 = uit
+        delay(40);
+    }
+    delay(5000); //5 second delay between each signal burst
+}
